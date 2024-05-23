e224d814029a46943c3b061327235e31ca0cd53c
Josiel Evangelista Junior <125618119+josielEJR@users.noreply.github.com>
2024-05-23 12:11:47 -0300

    Update clientes.sql

--- server/clientes.sql
+++ server/clientes.sql
@@ -1,13 +1,14 @@
 USE websiteInfo;
 
 CREATE TABLE IF NOT EXISTS clientes(
-    clienteID integer PRIMARY KEY AUTO_INCREMENT,
+    clienteID INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     senha VARCHAR(50) NOT NULL,
-    cpf VARCHAR(50) NOT NULL
+    cpf VARCHAR(50) NOT NULL,
+    celular VARCHAR(25) NOT NULL
 );
 
-INSERT INTO clientes ( clienteID, nome, email, senha, cpf) VALUES (001, 'Roberto', 'ralves@gmail.com', '123456', '85923791256');
+INSERT INTO clientes (  nome, email, senha, cpf) VALUES ('Roberto', 'ralves@gmail.com', '123456', '85923791256', '11953486834');
 
 -- CREATE TABLE IF NOT EXISTS produtos
